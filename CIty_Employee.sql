306677f9f9e39598621bd8bbe05421bb95268a7b
roman <roman-1901@mail.ru>
2023-05-04 21:32:39 +0300

    4

--- CIty_Employee.sql
+++ CIty_Employee.sql
@@ -25,18 +25,18 @@ UPDATE employee SET city_id = 4 WHERE id = 2;
 /*Получите имена и фамилии сотрудников, а также города, в которых они проживают.
 */
 SELECT e.first_name, e.last_name, c.city_name FROM employee e JOIN city c
-ON e.city_id = c.city_id
+                                                                   ON e.city_id = c.city_id
 
 /*Получите города, а также имена и фамилии сотрудников, которые в них проживают. Если в городе никто из сотрудников не живет, то вместо имени должен стоять null.
 */
 SELECT c.city_name, e.first_name, e.last_name FROM city c LEFT JOIN employee e
-ON e.city_id = c.city_id
+                                                                    ON e.city_id = c.city_id
 ORDER BY c.city_name
 
 /*Получите имена всех сотрудников и названия всех городов. Если в городе не живет никто из сотрудников, то вместо имени должен стоять null. Аналогично, если города для какого-то из сотрудников нет в списке, так же должен быть получен null.
 */
 SELECT e.first_name, c.city_name  FROM city c FULL JOIN employee e
-ON e.city_id = c.city_id
+                                                        ON e.city_id = c.city_id
 ORDER BY c.city_name
 
 /*Получите таблицу, в которой каждому имени должен соответствовать каждый город.
